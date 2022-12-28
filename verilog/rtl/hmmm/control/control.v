13a6308003f8310a025e2f83d4d37ad943d0b265
kavidey <kaviasher@gmail.com>
2022-12-28 11:23:49 -0800

    Switch control to simpler syntax

--- verilog/rtl/hmmm/control/control.v
+++ verilog/rtl/hmmm/control/control.v
@@ -4,117 +4,168 @@ module control
     input wire rst,
 
     // RAM
-    output wire mar_in, // 0
-    output wire mdr_in, // 1
-    output wire mdr_out, // 2
+    output reg mar_in,
+    output reg mdr_in,
+    output reg mdr_out,
 
     // Program Counter
-    output wire pc_out, // 3
-    output wire pc_jump, // 4
-    output wire pc_increment, // 5
+    output reg pc_out,
+    output reg pc_jump,
+    output reg pc_increment,
 
     // ALU
-    output wire tmp0_in, // 6
-    output wire tmp0_out, // 7
-    output wire alu_out, // 8
-    output wire [3:0] alu_op, // 9-12
-    output wire tmp1_in, // 13
-    output wire tmp1_out, // 14
+    output reg tmp0_in,
+    output reg tmp0_out,
+    output reg alu_out,
+    output reg [2:0] alu_op,
+    output reg tmp1_in,
+    output reg tmp1_out,
 
     // Register File
-    output wire [3:0] reg_sel, // 15-18
-    output wire reg_in, // 19
-    output wire reg_out, // 20
+    output reg [3:0] reg_sel,
+    output reg reg_in,
+    output reg reg_out,
 
     // Instruction Register
-    output wire ir_in, // 21
-    output wire ir_out, // 22
+    output reg ir_in,
+    output reg ir_out,
     input wire [15:0] ir_data,
 
     // IO
-    output wire in_out, // 23
-    output wire out_in, // 24
+    output reg in_out,
+    output reg out_in,
 
     // Halt
-    output wire halt, // 25
+    output reg halt,
 
     // Control
-    input wire [15:0] bus,
-    output wire [7:0] control_out
+    inout wire [15:0] bus
 );
     reg [2:0] microcode_instruction = 3'b0;
-    reg [24:0] control_reg;
 
-    assign mar_in = control_reg[0];
-    assign mdr_in = control_reg[1];
-    assign mdr_out = control_reg[2];
-    assign pc_out = control_reg[3];
-    assign pc_jump = control_reg[4];
-    assign pc_increment = control_reg[5];
-    assign tmp0_in = control_reg[6];
-    assign tmp0_out = control_reg[7];
-    assign alu_out = control_reg[8];
-    assign alu_op = control_reg[12:9];
-    assign tmp1_in = control_reg[13];
-    assign tmp1_out = control_reg[14];
-    assign reg_sel = control_reg[18:15];
-    assign reg_in = control_reg[19];
-    assign reg_out = control_reg[20];
-    assign ir_in = control_reg[21];
-    assign ir_out = control_reg[22];
-    assign in_out = control_reg[23];
-    assign out_in = control_reg[24];
-    assign halt = control_reg[25];
+    reg [15:0] control_out_reg;
+    reg control_out_enable;
+
+    assign bus = control_out_enable ? control_out_reg : {16{1'bZ}};
 
     always @(negedge clk) begin
+        // Control
+        control_out_enable <= 1'b0;
+        
+        // RAM
+        mar_in <= 1'b0;
+        mdr_in <= 1'b0;
+        mdr_out <= 1'b0;
+
+        // Program Counter
+        pc_out <= 1'b0;
+        pc_jump <= 1'b0;
+        pc_increment <= 1'b0;
+
+        // ALU
+        tmp0_in <= 1'b0;
+        tmp0_out <= 1'b0;
+        alu_out <= 1'b0;
+        tmp1_in <= 1'b0;
+        tmp1_out <= 1'b0;
+
+        // Register File
+        reg_in <= 1'b0;
+        reg_out <= 1'b0;
+
+        // Instruction Register
+        ir_in <= 1'b0;
+        ir_out <= 1'b0;
+
+        // IO
+        in_out <= 1'b0;
+        out_in <= 1'b0;
+
+        // Halt
+        halt <= 1'b0;
+        
         if (rst)
             microcode_instruction <= 3'b0;
         else
-            control_reg = 25'b0;
-
             if (microcode_instruction < 3'd2)
                 case(microcode_instruction)
                 3'd0: begin
-                    control_reg[3] = 1'b1; // pc_out
-                    control_reg[0] = 1'b1; // mar_in
-                    microcode_instruction = 3'd1;
+                    pc_out <= 1'b1; // pc_out
+                    mar_in <= 1'b1; // mar_in
+                    microcode_instruction <= 3'd1;
                 end
                 3'd1: begin
-                    control_reg[2] = 1'b1; // mdr_out
-                    contorl_reg[21] = 1'b1; // ir_in
-                    control_reg[5] = 1'b1; // pc_increment
-                    microcode_instruction = 3'd2;
+                    mdr_out <= 1'b1; // mdr_out
+                    ir_in <= 1'b1; // ir_in
+                    pc_increment <= 1'b1; // pc_increment
+                    microcode_instruction <= 3'd2;
                 end
+                default: ;
                 endcase
             else
                 case(ir_data[15:12])
-                4'b000: begin // halt, read, write, jump
+                4'b0000: begin // halt, read, write, jump
                     case(ir_data[1:0])
                     2'b00: begin // halt
-                        control_reg[25] = 1'b1; // halt
+                        halt <= 1'b1;
                     end
                     2'b01: begin // read
-                        control_reg[23] = 1'b1; // in_out
-                        control_reg[15:12] = ir_data[11:8]; // reg_sel
-                        control_reg[19] = 1'b1; // reg_in
+                        in_out <= 1'b1;
+                        reg_sel <= ir_data[11:8];
+                        reg_in <= 1'b1;
                     end
                     2'b10: begin // write
-                        control_reg[24] = 1'b1; // out_in
-                        control_reg[15:12] = ir_data[11:8]; // reg_sel
-                        control_reg[20] = 1'b1; // reg_out
+                        out_in <= 1'b1;
+                        reg_sel <= ir_data[11:8];
+                        reg_out <= 1'b1;
                     end
                     2'b11: begin // jump
-                        control_reg[4] = 1'b1; // pc_jump
-                        bus = {12'b0, ir_data[11:8]}; // bus
+                        pc_jump <= 1'b1;
+                        control_out_reg <= {12'b0, ir_data[11:8]};
+                        control_out_enable <= 1'b1;
+                    end
+                    endcase
+                    microcode_instruction <= 3'd0;
+                end
+                4'b0001: begin // setn
+                    reg_sel <= ir_data[11:8];
+                    reg_in <= 1'b1;
+                    control_out_reg <= {8'b0, ir_data[7:0]};
+                    control_out_enable <= 1'b1;
+                    microcode_instruction <= 3'd0;
+                end
+                4'b0010: begin // loadn
+                    case(microcode_instruction)
+                    3'd2: begin // move memory address to mar
+                        mar_in <= 1'b1; // mar_in
+                        control_out_enable <= 1'b1;
+                        control_out_reg <= {8'b0, ir_data[7:0]};
+                        microcode_instruction <= 3'd3;
+                    end
+                    3'd3: begin // move memory data to register
+                        mdr_out <= 1'b1;
+                        reg_sel <= ir_data[11:8];
+                        reg_in <= 1'b1;
+                        microcode_instruction <= 3'd0;
                     end
                     endcase
                 end
-                4'b001: begin // setn
-                    control_reg[18:15] = ir_data[11:8]; // reg_sel
-                    control_reg[19] = 1'b1; // reg_in
-                    bus = {8'b0, ir_data[7:0]}; // bus
+                4'b0011: begin // storen
+                    case(microcode_instruction)
+                    3'd2: begin // move memory address to mar
+                        mar_in <= 1'b1;
+                        control_out_enable <= 1'b1;
+                        control_out_reg <= {8'b0, ir_data[7:0]};
+                        microcode_instruction <= 3'd3;
+                    end
+                    3'd3: begin // move register to memory data
+                        mdr_in <= 1'b1;
+                        reg_sel <= ir_data[11:8];
+                        reg_out <= 1'b1;
+                        microcode_instruction <= 3'd0;
+                    end
+                    endcase
                 end
                 endcase
     end
-
 endmodule
---
+++ verilog/rtl/hmmm/control/control_tb.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 100ps
+
+module control_tb();
+    reg clk, rst;
+    wire [15:0] bus;
+
+    wire mar_in, mdr_in, mdr_out;
+    wire pc_out, pc_jump, pc_increment;
+    wire tmp0_in, tmp0_out, alu_out, tmp1_in, tmp1_out;
+    wire [2:0] alu_op;
+    wire [3:0] reg_sel;
+    wire reg_in, reg_out;
+    wire ir_in, ir_out;
+    wire in_out, out_in;
+    wire halt;
+
+
+    reg [15:0] fake_ir = 16'b0001_0001_0010_1010;
+
+    always begin
+        #5;
+        clk <= ~clk;
+    end
+
+    control uut (
+        .clk(clk),
+        .rst(rst),
+
+        .mar_in(mar_in),
+        .mdr_in(mdr_in),
+        .mdr_out(mdr_out),
+
+        .pc_out(pc_out),
+        .pc_jump(pc_jump),
+        .pc_increment(pc_increment),
+
+        .tmp0_in(tmp0_in),
+        .tmp0_out(tmp0_out),
+        .alu_out(alu_out),
+        .alu_op(alu_op),
+        .tmp1_in(tmp1_in),
+        .tmp1_out(tmp1_out),
+
+        .reg_sel(reg_sel),
+        .reg_in(reg_in),
+        .reg_out(reg_out),
+
+        .ir_in(ir_in),
+        .ir_out(ir_out),
+        .ir_data(fake_ir),
+
+        .in_out(in_out),
+        .out_in(out_in),
+
+        .halt(halt),
+
+        .bus(bus)
+    );
+
+    initial begin
+        $dumpfile("control_tb.vcd");
+        $dumpvars(0, control_tb);
+
+        rst <= 1'b0;
+        clk <= 1'b0;
+
+        rst <= 1'b1;
+        #10;
+        rst <= 1'b0;
+
+        #100
+
+        rst <= 1'b1;
+        #10;
+
+        $finish;
+    end
+
+endmodule
